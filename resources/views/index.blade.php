32b4674e4c14990f76852a15903ca591ce31abfc
Shakhawat Hosen <shakhawat9083@gmail.com>
2023-12-27 21:04:14 +0600

    change from shakhawat

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shakhawat</title>
+</head>
+<body>
+    <h1>Hi <font color='red'>Atik</font>, I am <strong>Shakhawat Hosen</strong></h1>
+</body>
+</html>
